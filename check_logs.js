1127d4c96a2497865b37bce34af53a3401c2db72
Linh Huynh <vanhuynh@ucdavis.edu>
2026-05-25 12:06:22 -0700

    deleted testing files

--- check_logs.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  page.on('console', msg => console.log('BROWSER LOG:', msg.text()));
-  page.on('pageerror', error => console.log('BROWSER ERROR:', error.message));
-  await page.goto('http://127.0.0.1:8080/Main/index.html', {waitUntil: 'networkidle0'});
-  await browser.close();
-})();
--- check_svg4.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 800 });
-  await page.goto('http://127.0.0.1:8080/Main/index.html', {waitUntil: 'networkidle0'});
-  await page.screenshot({ path: 'screenshot.png' });
-  await browser.close();
-})();
